022f4ecaa7e71cd244441c72ee4030846984e698
Liana Mancini <liana.mancini@gmail.com>
2018-06-30 20:54:54 -0400

    adds prod env variable

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
       "dialect": "mysql"
     },
     "production": {
-        "use_env_variable": "JAWSDB_URL",
+        "use_env_variable": "RDS_CONNECTION_URL",
         "dialect": "mysql"
     }
   }
