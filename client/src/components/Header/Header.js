fbba696ed9adc291e7157a6d2e100da4eb5478db
Liana Mancini <liana.mancini@gmail.com>
2018-07-17 21:10:05 -0400

    adds cost page

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = props => (
                 <NavDropdown eventKey={2} title="The Course">
                     <MenuItem eventKey={2.1} href="/background">Background</MenuItem>
                     <MenuItem eventKey={2.2} href="/course">The Course</MenuItem>
-                    {/* <MenuItem eventKey={2.3} href="/cost">Cost</MenuItem> */}
+                    <MenuItem eventKey={2.3} href="/cost">Cost</MenuItem>
                 </NavDropdown>
                 <NavItem eventKey={3} href="/FAQ">FAQ</NavItem>
                 <NavDropdown eventKey={4} title="About Me">
---
+++ client/src/pages/Cost/Cost.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Cost = () => (
+    <div className="container">
+        <h1>Cost</h1>
+        <h4>1L Fall Course</h4>
+        <p>Tuition for the course is $900. That amount will be due in two installments of $450 each via Venmo, PayPal, or check.</p>
+    </div>
+);
+
+export default Cost;
